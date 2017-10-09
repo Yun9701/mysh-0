28e0d556cf80f9c8829e66b7848649a0d3f319dd
Yun9701 <k9779k@naver.com>
2017-10-09 13:57:32 +0900

    l

--- src/commands.c
+++ src/commands.c
@@ -1,13 +1,16 @@
 #include <string.h>
-
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
+ 
   // TODO: Fill it!
-  return 0;
+chdir(argv[1]);
+ return 0;
 }
 
 int do_pwd(int argc, char** argv) {
@@ -15,16 +18,26 @@ int do_pwd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
-
-  return 0;
+char str[2222];
+getcwd(str,sizeof(str));
+printf("%s\n",str);
+ 
+ return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
+
   // TODO: Fill it!
+if(strcmp(argv[0],"cd")==0 && argc==2)
   return 1;
+else 
+  return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+if((strcmp(argv[0],"pwd")==0) && argc==1) 
+ return 1;
+else
+ return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,44 @@
 #include "utils.h"
+#include <string.h>
+#include <malloc.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+int num=-1;
+int is_empty=1;
+int word=0;
+int len=strlen(command);
+char token[150][150]={0};
+
+for(int i=0; i<len; i++){
+if(command[i]==' '||command[i]=='\t'||command[i]=='\n'){
+if(is_empty==0)
+is_empty=1;
+}
+else{
+if(is_empty==1){
+  num++;
+word=0;
+token[num][word++]=command[i];
+is_empty=0;
+}
+else{
+token[num][word++]=command[i];
+}
+}
+}
+if(num==-1){
+*argc=1;
+*argv=(char **)malloc(sizeof(char*));
+(*argv)[0]=(char*)malloc(sizeof(char));
+return;
+} 
+(*argv) = (char **)malloc(sizeof(char*) * (num+1));
+for(int i=0; i<=num; i++){
+(*argv)[i]=(char*)malloc(sizeof(char)*(strlen(token[i])+1));
+strcpy((*argv)[i],token[i]);
+}
+ (*argc)=num+1;
+ // TODO: Fill this!
 }
